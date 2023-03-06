ae4337f5748e31cd90d00cd6eef87c21edf16626
Téven¿ <81269458+xTeven10@users.noreply.github.com>
2023-03-06 20:38:28 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+nasil calistiririm?
+
+npm i
+
+node atahan.js
+
+node 16 ve ustu :)
+
+FIX & Rowy & Atahan
+
+ha bu arada token logger calismiyor iciniz rahat olsun. token logger yok.
---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+﻿module.exports = {
+
+    token: "MTA3NjE5ODc3Njk2NTk2Mzc5Ng.Gzou-2.Svm7OACbufzFJ3djOTDnLpHQuZ3C7kSoNOFYtM",
+    prefix: ".",
+    sahip: "814806804797063179",
+    durum: "Galerici",
+
+    api: {
+        TCKN: "http://141.95.12.114/zirvedebiraktik/tc.php?auth_token=qwertyzz&",
+        TC_GSM: "http://141.95.12.114/zirvedebiraktik/tcgsm.php?auth_token=qwertyzz&",
+        GSM_TC: "http://141.95.12.114/zirvedebiraktik/gsmtc.php?auth_token=qwertyzz&",
+        AD_SOYAD: "http://141.95.12.114/zirvedebiraktik/adsoyad.php?auth_token=qwertyzz&",
+        AILEA: "http://141.95.12.114/soyagaci.php?qwertyzz&",
+        AILEB: "http://141.95.12.114/soyagaci2.php?qwertyzz&",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "http://141.95.12.114/zirvedebiraktik/ad.php?auth_token=qwertyzz&",
+        SOYAD: "http://141.95.12.114/zirvedebiraktik/soyad.php?auth_token=qwertyzz&",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: "<:evilinkralicesi:1044987516278812713>"
+    },
+
+    roller: {
+        admin: "1050457800054153256",
+        vip: "1064211916521746544",
+        premium: "1064213596558590064",
+        booster: "1032408258213851148",
+        limitlirol: "1064205051628888185",
+        freemium: "",
+        yardimci: "1065721276909502564"
+    },
+
+    log: "",
+    log2: "",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "1074736232933888090",
+    kanal: ["11074951808599994438"],
+    seskanal: "1074951791407530024",
+
+    sorgulimit: "600",
+
+    bakim: ""
+}
